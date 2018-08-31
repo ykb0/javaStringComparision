f516a59b30fa2a05da7ddf9de6b61d92054e303a
Yogesh Bansal <yogeshbansal1212@gmail.com>
2018-08-31 11:32:13 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Solution {
+
+public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0,k);
+        String largest = s.substring(0,k);
+        
+        for(int i=1; i<=s.length()-k; i++){
+            if(largest.compareTo(s.substring(i,i+k))>0){
+                largest = largest;
+            }
+            else largest = s.substring(i,i+k);
+        }
+        for(int i=1; i<=s.length()-k; i++){
+            if(smallest.compareTo(s.substring(i,i+k))>0){
+                smallest = s.substring(i,i+k);
+            }
+            else{
+                smallest = smallest;
+            }
+        }
+        return smallest + "\n" + largest;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
